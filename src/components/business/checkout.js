85b5c1a0afee2b25e3124d7b37aba062b204c6b1
lmjing <lmjing1234@gmail.com>
2019-07-24 18:13:43 +0900

    반납 기능 개발 중

--- src/components/business/checkout.js
+++ src/components/business/checkout.js
@@ -5,35 +5,15 @@ import { replaceRow } from "./list";
  * @description 도서 대여를 수행 (반납일 : 대여일로부터 2주 후)
  * @author: mijeong lee
  */
-export const makeModal = (index) => {
-    // 수 2자리로 만들기
-    const twoLength = (n) => {
-        return (n < 10 ? '0' : '') + n;
-    };
-    // 반납 예정일 리턴 함수
-    const returnDate = () => {
-        // 반납일 2주 후
-        let date = new Date();
-        date.setDate(new Date().getDate() + 14);
-        // 자바스크립트의 월은 0부터 시작하기 때문에 1을 더해야 정상적인 월이 됩니다.
-        return `${date.getFullYear()}-${twoLength(date.getMonth() + 1)}-${twoLength(date.getDate())}`;
-    };
 
+
+const Modal = (index) => {
     let modalEl = document.getElementById("modal");
-    let rDate = returnDate();
     // 취소 버튼 event 등록
     document.getElementById("cancel").addEventListener("click", (e) => {
         e.preventDefault();
         modal.close();
     }, false);
-    // 대여 버튼 event 등록
-    document.getElementById("confirm").addEventListener("click", (e) => {
-        e.preventDefault();
-        __book_data[index].rtn_dt = rDate;
-        __book_data[index].sttus = '반납예정';
-        replaceRow(index);
-        modal.close();
-    }, true);
 
     const modal = {
         center: () => {
@@ -44,8 +24,6 @@ export const makeModal = (index) => {
             modalEl.style.left = `${left + window.scrollX}px`;
         },
         open: () => {
-            let pNode = document.getElementById('rDate');
-            pNode.innerText = '대여시 반납 예정일 : ' + rDate;
             // 모달 보이게 변경
             modalEl.style.display = "block";
             modal.center();
@@ -59,3 +37,192 @@ export const makeModal = (index) => {
     };
     return modal;
 };
+
+export const CheckoutModal = (index) => {
+    // 수 2자리로 만들기
+    const twoLength = (n) => {
+        return (n < 10 ? '0' : '') + n;
+    };
+    // 반납 예정일 리턴 함수
+    const returnDate = () => {
+        // 반납일 2주 후
+        let date = new Date();
+        date.setDate(new Date().getDate() + 14);
+        // 자바스크립트의 월은 0부터 시작하기 때문에 1을 더해야 정상적인 월이 됩니다.
+        return `${date.getFullYear()}-${twoLength(date.getMonth() + 1)}-${twoLength(date.getDate())}`;
+    };
+
+    const checkout = () => {
+        __book_data[index].rtn_dt = rDate;
+        __book_data[index].sttus = '반납예정';
+    };
+
+    let modal = Modal(index);
+    let modalEl = document.getElementById("modal");
+    let rDate = returnDate();
+
+    // 대여 버튼 event 등록
+    let confirmBtn = document.getElementById("confirm");
+    confirmBtn.innerText = '대여';
+    confirmBtn.addEventListener("click", (e) => {
+        e.preventDefault();
+        checkout();
+        replaceRow(index);
+        modal.close();
+    }, true);
+
+    // 모달 내용 변경
+    let titleNode = modalEl.querySelector('.title');
+    titleNode.innerText = '대여하시겠습니까?';
+    let rDateNode = document.getElementById('rDate');
+    rDateNode.innerText = '대여시 반납 예정일 : ' + rDate;
+
+    return modal;
+};
+
+export const ReturnModal = (index) => {
+    let modalEl = document.getElementById("modal");
+    // 취소 버튼 event 등록
+    document.getElementById("cancel").addEventListener("click", (e) => {
+        e.preventDefault();
+        modal.close();
+    }, false);
+    // 대여 버튼 event 등록
+    let confirmBtn = document.getElementById("confirm");
+    confirmBtn.innerText = '반납';
+    confirmBtn.addEventListener("click", (e) => {
+        e.preventDefault();
+        __book_data[index].rtn_dt = '';
+        __book_data[index].sttus = '대여가능';
+        replaceRow(index);
+        modal.close();
+    }, true);
+
+    const modal = {
+        center: () => {
+            // 브라우저 창의 위쪽과 왼쪽으로부터 모달 창까지의 거리를 계산한다.
+            var top = Math.max(window.innerHeight - modalEl.offsetHeight, 0) / 2,
+                left = Math.max(window.innerWidth - modalEl.offsetWidth, 0 ) / 2;
+            modalEl.style.top = `${top + window.scrollY}px`;
+            modalEl.style.left = `${left + window.scrollX}px`;
+        },
+        open: () => {
+            let titleNode = modalEl.querySelector('.title');
+            titleNode.innerText = '반납하시겠습니까?';
+            // 모달 보이게 변경
+            modalEl.style.display = "block";
+            console.log('test');
+            modal.center();
+
+            window.addEventListener('resize', modal.center);
+            document.addEventListener('scroll', modal.center);
+        },
+        close: () => {
+            modalEl.style.display = "none";
+        },
+    };
+    return modal;
+};
+
+// export const CheckoutModal = (index) => {
+//     // 수 2자리로 만들기
+//     const twoLength = (n) => {
+//         return (n < 10 ? '0' : '') + n;
+//     };
+//     // 반납 예정일 리턴 함수
+//     const returnDate = () => {
+//         // 반납일 2주 후
+//         let date = new Date();
+//         date.setDate(new Date().getDate() + 14);
+//         // 자바스크립트의 월은 0부터 시작하기 때문에 1을 더해야 정상적인 월이 됩니다.
+//         return `${date.getFullYear()}-${twoLength(date.getMonth() + 1)}-${twoLength(date.getDate())}`;
+//     };
+//
+//     let modalEl = document.getElementById("modal");
+//     let rDate = returnDate();
+//     // 취소 버튼 event 등록
+//     document.getElementById("cancel").addEventListener("click", (e) => {
+//         e.preventDefault();
+//         modal.close();
+//     }, false);
+//     // 대여 버튼 event 등록
+//     let confirmBtn = document.getElementById("confirm");
+//     confirmBtn.innerText = '대여';
+//     confirmBtn.addEventListener("click", (e) => {
+//         e.preventDefault();
+//         __book_data[index].rtn_dt = rDate;
+//         __book_data[index].sttus = '반납예정';
+//         replaceRow(index);
+//         modal.close();
+//     }, true);
+//
+//     const modal = {
+//         center: () => {
+//             // 브라우저 창의 위쪽과 왼쪽으로부터 모달 창까지의 거리를 계산한다.
+//             var top = Math.max(window.innerHeight - modalEl.offsetHeight, 0) / 2,
+//                 left = Math.max(window.innerWidth - modalEl.offsetWidth, 0 ) / 2;
+//             modalEl.style.top = `${top + window.scrollY}px`;
+//             modalEl.style.left = `${left + window.scrollX}px`;
+//         },
+//         open: () => {
+//             let titleNode = modalEl.querySelector('.title');
+//             titleNode.innerText = '대여하시겠습니까?';
+//             let rDateNode = document.getElementById('rDate');
+//             rDateNode.innerText = '대여시 반납 예정일 : ' + rDate;
+//             // 모달 보이게 변경
+//             modalEl.style.display = "block";
+//             modal.center();
+//
+//             window.addEventListener('resize', modal.center);
+//             document.addEventListener('scroll', modal.center);
+//         },
+//         close: () => {
+//             modalEl.style.display = "none";
+//         },
+//     };
+//     return modal;
+// };
+//
+// export const ReturnModal = (index) => {
+//     let modalEl = document.getElementById("modal");
+//     // 취소 버튼 event 등록
+//     document.getElementById("cancel").addEventListener("click", (e) => {
+//         e.preventDefault();
+//         modal.close();
+//     }, false);
+//     // 대여 버튼 event 등록
+//     let confirmBtn = document.getElementById("confirm");
+//     confirmBtn.innerText = '반납';
+//     confirmBtn.addEventListener("click", (e) => {
+//         e.preventDefault();
+//         __book_data[index].rtn_dt = '';
+//         __book_data[index].sttus = '대여가능';
+//         replaceRow(index);
+//         modal.close();
+//     }, true);
+//
+//     const modal = {
+//         center: () => {
+//             // 브라우저 창의 위쪽과 왼쪽으로부터 모달 창까지의 거리를 계산한다.
+//             var top = Math.max(window.innerHeight - modalEl.offsetHeight, 0) / 2,
+//                 left = Math.max(window.innerWidth - modalEl.offsetWidth, 0 ) / 2;
+//             modalEl.style.top = `${top + window.scrollY}px`;
+//             modalEl.style.left = `${left + window.scrollX}px`;
+//         },
+//         open: () => {
+//             let titleNode = modalEl.querySelector('.title');
+//             titleNode.innerText = '반납하시겠습니까?';
+//             // 모달 보이게 변경
+//             modalEl.style.display = "block";
+//             console.log('test');
+//             modal.center();
+//
+//             window.addEventListener('resize', modal.center);
+//             document.addEventListener('scroll', modal.center);
+//         },
+//         close: () => {
+//             modalEl.style.display = "none";
+//         },
+//     };
+//     return modal;
+// };
--- src/components/business/list.js
+++ src/components/business/list.js
@@ -4,30 +4,40 @@
  * @author yejin kim
  */
 import { __book_data } from "../data/book.js"
-import { makeModal } from "./checkout.js"
+import { CheckoutModal, ReturnModal } from "./checkout.js"
 
-export const makeList = () => {
-  let template = "";
-  let btnRows = [];
-  let book = __book_data.map((book, i) => {
-    if (book.rtn_dt === "") { btnRows.push(i); }
-    template +=
-      `<div class="row row__list">
+let ableChoutRows = [];
+let ableReturnRows = [];
+
+const Item = (book, i) => {
+  const item = {
+    template:  `<div class="row row__list">
         <div class="col-md-1">${book.num}</div>
         <div class="col-md-4">${book.name}</div>
         <div class="col-md-2">${book.auth}</div>
         <div class="col-md-2">${book.pub}</div>
-        <div class="col-md-1">${book.rtn_dt != "" ? `${book.sttus}` : `<button class="checkout">대여</button>`}</div>
+        <div class="col-md-1">${book.rtn_dt !== "" ? `<button class="return">반납</button>` : `<button class="checkout">대여</button>`}</div>
         <div class="col-md-2">${book.rtn_dt}</div>
-      </div>`;
+      </div>`,
+    modal: book.rtn_dt !== "" ? ReturnModal(i) : CheckoutModal(i),
+  };
+  return item;
+};
+
+let itemList = [];
+export const makeList = () => {
+  let template = "";
+  let book = __book_data.map((book, i) => {
+    let item = Item(book, i);
+    itemList.push(item);
+    template += item.template;
   });
   document.getElementById("result").innerHTML = template;
-  let buttons = document.getElementsByClassName("checkout");
+  let buttons = document.getElementsByTagName('button');
   for(let i = 0; i < buttons.length; i++) {
-    let row = btnRows[i];
     buttons[i].addEventListener("click", () => {
-      let modal = makeModal(row);
-      modal.open("settings");
+      let modal = itemList[i].modal;
+      modal.open();
     }, false);
   }
 };
@@ -40,7 +50,7 @@ export const replaceRow = (index) => {
        <div class="col-md-4">${book.name}</div>
        <div class="col-md-2">${book.auth}</div>
        <div class="col-md-2">${book.pub}</div>
-       <div class="col-md-1">${book.rtn_dt !== "" ? `${book.sttus}` : `<button class="checkout">대여</button>`}</div>
+       <div class="col-md-1">${book.rtn_dt != "" ? `<button class="return">반납</button>` : `<button class="checkout">대여</button>`}</div>
        <div class="col-md-2">${book.rtn_dt}</div>`;
 
   document.getElementById("result").childNodes[index].innerHTML = template;
